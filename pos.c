0ca658cf80fd75ade053b7d8fff0abc2ca3d5f32
defoebosco <53421388+defoebosco@users.noreply.github.com>
2019-07-29 11:44:08 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+    if(n<0)
+    printf("negative");
+    else
+    printf("Postivie");
+}
